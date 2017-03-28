236e2a1077381c8a6da62e067253f3883ee83f4d
Yudong Diao <ydiao@umass.edu>
2017-03-27 20:58:35 -0400

    fixed create-event.js

--- app/components/create-event.js
+++ app/components/create-event.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Right_Bar from './right-bar';
 import Nav_Bar from './nav-bar';
-//import ReacrDOM from 'react-dom';
 
 export default class creatEvent extends React.Component {
   render() {
