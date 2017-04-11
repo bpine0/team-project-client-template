4f6548cc05e3f97d30a645fe2a4cb1e890a84439
Nicholas Bertrand <nbertrand@umass.edu>
2017-04-11 16:47:55 -0400

    modified link-list.js to be more similar to commenthread.js

--- app/components/link-list.js
+++ app/components/link-list.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import EventLink from './event-link';
 
 export default class LinkList extends React.Component {
   render() {
     return (
-    <div>
-      <div>
-      <li className= "media">
-        <EventLink name = "Supplemental Instruction Review" info = "UMass LRC">
-        </EventLink>
-        </li>
-      </div>
-
-      <div>
-      <li className= "media">
-        <EventLink name = "UMass vs. BC Hockey Game" info = "UMass Althetics">
-        </EventLink>
-        </li>
-      </div>
-
-      <div>
-      <li className= "media">
-        <EventLink name = "Campus Barbeque" info = "UMass Alumni Association">
-        </EventLink>
-        </li>
-      </div>
-
-
-
-
-    </div>
+      <ul className="media-list">
+          {React.Children.map(this.props.children, function(child) {
+            return (
+              <li className="media">
+                {child}
+              </li>
+            )
+          })}
+        </ul>
 
 
     )
