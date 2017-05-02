d8fa2f8d2473675568bec4da15ae6e0bb376f206
Jason Rowley <jasonrowley@verizon.net>
2017-05-02 12:55:49 -0400

    added space between event and org links

--- client/app/components/event-entry.js
+++ client/app/components/event-entry.js
@@ -15,7 +15,7 @@ export default class Event extends React.Component {
                     <img className="media-object" src={this.props.event_image} style={{height: 150}}/>
                   </div>
                   <div className="media-body">
-                    <h3 className = "media-heading entry-heading"><Link to={"/event-details/" + this.props._id} className="event-links">{this.props.event_name}</Link><a href={this.props.org_link}>{this.props.org}</ a>
+                    <h3 className = "media-heading entry-heading"><Link to={"/event-details/" + this.props._id} className="event-links">{this.props.event_name}</Link> <a href={this.props.org_link}>{this.props.org}</ a>
                       <span className="input-group-btn">
                         <button className="btn btn-default pull-right favorite-event" type="button">
                           <span className="glyphicon glyphicon-star-empty favorite-text"></span>
